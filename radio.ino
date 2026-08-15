2ffb6ec1a702a0d834658d775707711668be6c25
jschuetter <100084569+jschuetter@users.noreply.github.com>
2026-08-15 11:58:27 -0400

    Initial commit

---
+++ radio.ino
@@ -0,0 +1,119 @@
+// TEA7567 setup
+#include <Wire.h>
+#include <TEA5767.h>
+
+// Optional: this callback function can be passed to the TEA5767 constructor.
+// It is invoked whenever the radio state is updated.
+// You can use it to display the current information on an external
+// module (e.g., OLED/LCD) or simply log it to the Serial monitor
+// for debugging, as shown here.
+void radioPrintInfo(const tea5767_info_t *info);
+
+// Optional: this callback function can be passed to the TEA5767 constructor.
+// You can override only the settings you care about here, leaving
+// all other values at their defaults. For example, you might want
+// to change the band limits, mute setting, or de-emphasis time.
+// This way, you avoid having to manually initialize every property.
+void radioUpdateConfig(tea5767_config_t *config);
+
+TEA5767 radio(&radioPrintInfo, &radioUpdateConfig);
+float foundStations[10];
+int stationCount;
+double currentFreq = 90.9;
+
+// SSD1306 setup
+#include <Arduino.h>
+#include <SPI.h>
+#include <U8g2lib.h>
+
+// U8G2_SSD1306_128X64_NONAME_1_4W_SW_SPI u8g2(U8G2_R0, /* clock=*/ 13, /* data=*/ 11, /* cs=*/ 10, /* dc=*/ 9, /* reset=*/ 8);
+U8G2_SH1106_128X64_NONAME_2_HW_I2C u8g2(U8G2_R0, U8X8_PIN_NONE);
+
+
+// Rotary encoder setup
+#include <Encoder.h>
+
+Encoder enc(3,2);
+long encPos = 0;
+const unsigned long debounceMs = 250;
+unsigned long lastEventMs = 0;
+const int countPerDetent = 4;
+const double mhzInc = 0.2;
+
+void setup() {
+  // Get serial output
+  Serial.begin(9600);
+
+  // RADIO
+  Wire.begin();
+  radio.awake();
+  radio.setFrequency(currentFreq);
+
+  // DISPLAY
+  u8g2.begin();
+
+}
+
+void loop() {
+  long newPos = enc.read();
+  if (newPos != encPos) {
+    unsigned long t = millis();
+    if (t - lastEventMs >= debounceMs) {
+      // Increment current frequency by number of detents
+      double freqInc = (newPos > encPos) ? ceil(double(newPos - encPos) / countPerDetent) : floor(double(newPos - encPos) / countPerDetent);
+      double newFreq = currentFreq + mhzInc * freqInc;
+      Serial.print("Enc delta: ");
+      Serial.println(double(newPos - encPos)/countPerDetent);
+      Serial.print("New freq: ");
+      Serial.println(newFreq);
+      // Make sure new frequency is valid
+      if (newFreq <= 108.0 && newFreq >= 88.0) {
+        currentFreq = newFreq;
+        radio.setFrequency(currentFreq);
+      }
+      lastEventMs = millis();
+    }
+    encPos = newPos;
+  }
+  
+  char freqStr[5];
+  dtostrf(currentFreq, 3, 1, freqStr);
+  char freq[15];
+  sprintf(freq, "%s MHz", freqStr);
+  
+  u8g2.firstPage();
+  do {
+    u8g2.setFont(u8g2_font_osr35_tn);
+    u8g2.drawStr(64-(u8g2.getStrWidth(freqStr)/2),50,freqStr);
+    u8g2.setFont(u8g2_font_timR10_tr);
+    u8g2.drawStr(98,64,"MHz");
+  } while ( u8g2.nextPage() );
+}
+
+void radioPrintInfo(const tea5767_info_t *info) {
+  Serial.println("**********");
+  if (info->muted) {
+    Serial.println("Muted!");
+  } else if (info->standby) {
+    Serial.println("Standby!");
+  } else {
+    if (info->searching) {
+      Serial.println("Searching...");
+    } else {
+      Serial.print(info->mhz);
+      Serial.println("MHz");
+    }
+  }
+  Serial.print("Stereo: ");
+  Serial.println(info->stereo ? "Stereo" : "Mono");
+  Serial.print("Tune: ");
+  Serial.println(info->tune);
+  Serial.print("Signal level: ");
+  Serial.print(info->signal_level);
+  Serial.println("%");
+  Serial.println("**********");
+}
+
+void radioUpdateConfig(tea5767_config_t *config) {
+  config->standby = false;
+}
